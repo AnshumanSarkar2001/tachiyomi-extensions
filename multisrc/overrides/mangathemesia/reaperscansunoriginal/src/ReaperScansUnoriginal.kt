b4ccbb317b107c11f8fdfd3efa8a79e96d1f5686
AnshumanSarkar2001 <anshumansarkar2015@gmail.com>
2023-12-30 20:54:16 +0530

    Add Reaper Scans Unoriginal

---
+++ multisrc/overrides/mangathemesia/reaperscansunoriginal/src/ReaperScansUnoriginal.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.reaperscansunoriginal
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+
+class ReaperScansUnoriginal : MangaThemesia("Reaper Scans (unoriginal)", "https://reaper-scans.com", "en")
